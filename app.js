3d5ba50a4c16a62f4b455d4372d79bfff115bda8
hkharshita <komraharshita@gmail.com>
2024-08-06 23:50:47 +0530

    first commit

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 
 const express=require('express');
 const app=express();
-app.use((req,res,next)=>{
- console.log("In the middleware!");
- next();
+const bodyParser=require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/add-product',(req,res,next)=>{
+ 
+//res.send('<h1>In the "Add product" page!');
+res.send('<form action="/product" method="POST" ><input type="text" name="Item"><br><br><input type="text" name="size"><button type="submit">Add product</button></form>');
+ 
 })
-app.use((req,res,next)=>{
-    console.log("In the another middleware!");
-    res.send('<h1>Hello from Express!</h1>');
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body); //undefined
+res.redirect('/');
+})
+app.use('/',(req,res,next)=>{
+   
+    res.send('<h1>HOME PAGE</h1>');
 })
 
 app.listen(4000);
