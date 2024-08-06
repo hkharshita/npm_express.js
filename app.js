a8c45f41713d680d44a443f680b84ee5628b04e5
hkharshita <komraharshita@gmail.com>
2024-08-06 19:45:52 +0530

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const http = require('http');
 
-const routes=require("./routes");
-const server = http.createServer(routes);
-server.listen(4000);
+const express=require('express');
+const app=express();
+app.use((req,res,next)=>{
+ console.log("In the middleware!");
+ next();
+})
+app.use((req,res,next)=>{
+    console.log("In the another middleware!");
+    res.send('<h1>Hello from Express!</h1>');
+})
+
+app.listen(4000);
