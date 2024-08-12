52fa48fa957cf92f56f505c1cae8f5527a2a1fd9
hkharshita <komraharshita@gmail.com>
2024-08-12 23:37:45 +0530

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+const path=require('path');
 const express = require('express');
 const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
+const contactRoutes=require('./routes/contact');
 
 const app = express();
 const bodyParser = require('body-parser');
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/shop',shopRoutes);
 app.use('/admin',adminRoutes);
+app.use('/contactus',contactRoutes);
+app.use('/success',(req,res)=>{
+res.sendFile(path.join(__dirname,'views','success.html'));
+})
 app.use((req,res,next)=>{
-   res.status(404).send('<h1>Page not found!</h1>');
+   res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
+const path=require('path');
 const express=require('express');
 const router=express.Router();
-
+const rootDir=require('../util/path.js');
 router.get('/add-product', (req, res, next) => {
 
     //res.send('<h1>In the "Add product" page!');
-    res.send('<form action="/admin/add-product" method="POST" ><input type="text" name="Item"><br><br><input type="text" name="size"><br><br><button type="submit">Add product</button></form>');
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 
 })
 
